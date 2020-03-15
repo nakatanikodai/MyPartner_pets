ecee75abd2f20e28a1b52e5a0847c664aae62409
nakatanikodai <nakatanixx1123@gmail.com>
2020-03-15 19:40:02 +0900

    一覧表示とページネーションの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,16 @@
 class PostsController < ApplicationController
   
   def index
-    @posts = Post.all
+    @posts = Post.all.limit(6).page(params[:page]).per(6)
   end
 
   def new
+    @post = Post.new
     
+  end
+
+  def create
+    Post.create(post_params)
   end
 
   def edit
@@ -13,4 +18,13 @@ class PostsController < ApplicationController
 
   def show
   end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :image, :content,)
+  end
 end
+
+
+
