28f3fb231ac55fdcafd749f540b4008b88edf7dd
nakatanikodai <nakatanixx1123@gmail.com>
2020-04-30 02:40:41 +0900

    投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,7 +38,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.permit(:partner, :title, :image, :content ).merge(user_id: current_user.id)
+    params.require(:post).permit(:partner, :title, :image, :content ).merge(user_id: current_user.id)
   end
 end
 
